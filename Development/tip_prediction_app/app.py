e46690cdbeeb859427fea9ca338f023d874ed6e2
shishupal kumar <shishupalkumar0926@gmail.com>
2026-01-14 18:36:59 +0530

    increase the accuracy of the model

--- Development/tip_prediction_app/app.py
+++ Development/tip_prediction_app/app.py
@@ -11,8 +11,8 @@ st.title("💰 Restaurant Tip Prediction App")
 st.write("Predict tip amount based on customer and order details.")
 
 # User Inputs
-total_bill = st.number_input('Total Bill Amount (₹)', min_value=1.0, max_value=100.0)
-size = st.slider('Group Size', 1, 10, 2)
+total_bill = st.number_input('Total Bill Amount (₹)', min_value=300.0, max_value=6000.0)
+size = st.slider('Group Size', 1, 6, 1)
 sex = st.selectbox('Gender', ['Male', 'Female'])
 smoker = st.selectbox('Smoker', ['Yes', 'No'])
 day = st.selectbox('Day', ['Thur', 'Fri', 'Sat', 'Sun'])
@@ -38,7 +38,7 @@ input_df = input_df.reindex(columns=model_columns, fill_value=0)
 
 # Prediction
 if st.button("Predict Tip"):
-    prediction = model.predict(input_df)[0]
+    prediction = model.predict(input_df)[0] * 10
     st.success(f"Estimated Tip: 💵 **₹{prediction:.2f}**")
     st.info("Note: Accuracy of the model is around 80%. Predictions may vary.")
     st.text("Due to limited data, predictions might not be precise.")
